0a1914efb24cea564d004d71f4916aafe46d416d
Mike-MK <michaelmukiri99@gmail.com>
2022-01-24 10:15:15 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Markdown Engine
+A python command line app that translates markdown syntax to html. 
+
+# Description 
+The project uses regex to change a sub-set of markdown syntax including:
+- Headers 1,2,3
+- Bold 
+- Italic
+- Deleted
+- Links
+
+# Installation
+To run this project you need
+    - Python 2.7 or later
+    
+# How to Use
+Run the app in command line, passing the full path of a single markdown file as a parameter using --file or -f flag. The result is stored in the output folder and can be viewed in any browser of your choice.
---
+++ main.py
@@ -0,0 +1,66 @@
+import re
+import argparse
+import ntpath
+
+html_base='''<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Document</title>
+    </head>
+    <body>
+'''
+html_close='''</body>
+</html>'''
+
+
+RULES = {
+    "### (?P<header>.*)?" : "<h3>\g<header></h3>",
+    "## (?P<header>.*)?" : "<h2>\g<header></h2>",
+    "# (?P<header>.*)?" : "<h1>\g<header></h1>",
+    "\*\*(?P<bold>.*?)\*\*" : "<b>\g<bold></b>",
+    "\*(?P<italic>.*?)\*" : "<i>\g<italic></i>",
+    "~~(?P<deleted>.*?)~~" : "<del>\g<deleted></del>",
+    "\[(?P<title>.*?)\]\((?P<link>.*?)\)" : '<a href="\g<link>">\g<title></a>',
+
+    } 
+
+def main():
+    parse = argparse.ArgumentParser(description='Markdown to html parser')
+    parse.add_argument('--file','-f',type=str,help='Path to the markdown file')
+    args = parse.parse_args()
+
+    if not args.file or not args.file.endswith('.md'):
+        print("Invalid argument passed.Exiting...")
+        return 
+    
+    try:
+        with open(args.file) as file:
+            doc = file.read()
+
+        for key in RULES.keys():
+            doc = re.sub(key,RULES[key],doc,re.M)
+
+        output_path = ntpath.basename(args.file)
+        with open(f'output/{output_path}.html','w')as output:
+            output.write(html_base)
+            output.write(doc)
+            output.write(html_close)
+
+        print('File converted successfully')
+        return 
+        
+    except Exception as e:
+        print(e)
+        return
+
+if __name__=='__main__':
+    main()
+    
+
+    
+    
+   
